b96feacb3452514ad893fdada8e0ace17f51ffc9
PhilTrudel <102983627+ptrudel-blanko@users.noreply.github.com>
2022-06-09 17:33:42 -0400

    09 juin 2022

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+//Introduction
+let js = 'amazing'
+console.log(40 + 8 + 23 - 10);
+
+//Values and Variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Mathilda';
+
+console.log(firstName);
