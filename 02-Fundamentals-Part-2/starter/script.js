f34f00c82a33f3e5386eb73e48423ad5ea57df40
Philippe Trudel <ptrudel@blanko.ca>
2022-07-07 16:56:02 -0400

    Progrès

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -54,31 +54,120 @@
 // console.log(`${phil.firstName} has ${phil.friends.length} friends, and his best friend is called ${phil.friends[0]}.`)
 
 
-const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
 
-    calcBMI: function () {
-        return (this.mass / this.height ** 2).toFixed(2);
-    }
+//     calcBMI: function () {
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi;
+//     }
+// };
+
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+
+//     calcBMI: function () {
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi;
+//     }
+// };
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// if (mark.bmi > john.bmi) {
+//     console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})!`)
+// } else {
+//     console.log(`John's BMI (${john.bmi}) is higher than Marks's (${mark.bmi})!`)
+// }
+
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 💪`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`Lifting weights repetition ${rep} 💪`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`)
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log('This is a 6');
+// }
+
+// const philArray = [
+//     'Philippe',
+//     'Trudel',
+//     1997,
+//     'Web developper',
+//     ['David', 'Raphaël', 'Vincent'],
+// ];
+
+// for (let i = philArray.length - 1; i >= 0; i--) {
+//     console.log(philArray[i]);
+// }
+
+// const types = [];
+
+
+//     types[i] = typeof philArray[i];
+// }
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2037 - years[i]);
+// }
+
+// console.log(ages);
+
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. 
+Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-console.log(mark.calcBMI());
 
-const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-    calcBMI: function () {
-        return (this.mass / this.height ** 2).toFixed(2);
-    }
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
 }
-
-console.log(john.calcBMI());
-
-if (mark.calcBMI() > john.calcBMI()) {
-    console.log(`Mark's BMI (${mark.calcBMI()}) is higher than John's (${john.calcBMI()})!`)
-} else {
-    console.log(`John's BMI (${john.calcBMI()}) is higher than Marks's (${mark.calcBMI()})!`)
-}
+console.log(bills, tips, totals);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 13;
+
+
+// document.querySelector('.guess').value = 13;
+
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+
+document.querySelector('.check').addEventListener
+    ('click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+
+        if (!guess) {
+            document.querySelector('.message').textContent = 'No number! ⛔';
+        } else if (guess === number) {
+            document.querySelector('.message').textContent = 'Good number! 👍';
+            document.querySelector('.number').textContent = number;
+            document.body.style.backgroundColor = "#60b347";
+        } else if (guess > number) {
+            if (score > 0) {
+                document.querySelector('.message').textContent = 'Number too high!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the game!';
+                document.body.style.backgroundColor = "#F0706A";
+            }
+        } else if (guess < number) {
+            if (score > 0) {
+                document.querySelector('.message').textContent = 'Number too low!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = 'You lost the game!';
+                document.body.style.backgroundColor = "#F0706A";
+            }
+        }
+
+        console.log(guess);
+    });
